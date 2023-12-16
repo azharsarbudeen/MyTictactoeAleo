c3110cc262c5a53293de757cae25d709932fe026
azharsarbudeen <108316660+azharsarbudeen@users.noreply.github.com>
2023-12-16 18:17:51 +0000

    Pending changes exported from your codespace

---
+++ setup_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update and upgrade
+sudo apt update && sudo apt upgrade -y
+
+# Install git
+sudo apt-get install git-all -y
+
+# Install curl
+sudo apt-get install curl -y
+
+# Rust installation
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Note: The Rust installation script may prompt for user input. Make sure to read and follow any instructions it provides.
+
+# Switch to root user
+sudo su
